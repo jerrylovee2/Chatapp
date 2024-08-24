9f3858d3380038601181b335c3735bd5d0db5934
jerry <jerrylovee2@gmail.com>
2024-08-24 10:21:12 +0530

    Change - Formating of json response

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -2,21 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 const Message = ({ message }) => {
   const [parsedMessage, setParsedMessage] = useState(null);
-
+  console.log(message)
   useEffect(() => {
     try {
       const parsed = JSON.parse(message);
-      setParsedMessage(parsed);
+      setParsedMessage(JSON.stringify(parsed.body, null, 2));
     } catch (error) {
       console.error("Failed to parse message:", error);
       setParsedMessage(message); 
     }
   }, [message]);
 
+  console.log(JSON.stringify(parsedMessage))
   return (
     <div>
       {parsedMessage ? (
-        <pre>{JSON.stringify(parsedMessage, null, 2)}</pre>
+        <pre>{parsedMessage}</pre>
       ) : (
         <p>{message}</p>
       )}
